c8c78b8c43e8d3cb0eda9620c4708c24275b4e7e
Raphael Bernardo <raphaelbgr@gmail.com>
2015-10-05 02:20:44 -0300

    Atribute added

--- src/app/model/clients/Client.java
+++ src/app/model/clients/Client.java
@@ -49,6 +49,7 @@ public class Client implements Serializable {
 	String googleplus			= null;
 	String youtube				= null;
 	String msn					= null;
+	String platform				= null;
 	
 	Message lastMessage 		= null;
 	List<Message> unSentMsgs	= null;
@@ -81,8 +82,17 @@ public class Client implements Serializable {
 		this.membertype 		= m.getType();
 		this.lastMessage 		= m;
 	}
-	public Client(String string) {
-		this.name = string;
+	public Client(String name) {
+		this.name = name;
+	}
+	public Client(String login, String password) {
+		if (login.contains("@"))
+		{
+			this.email = login;
+		} else {
+			this.login = login;
+		}
+		this.password = password;
 	}
 	public Long getId() {
 		return id;
@@ -380,6 +390,22 @@ public class Client implements Serializable {
 	}
 	public void setDisconnect(boolean isDisconnect) {
 		this.isDisconnect = isDisconnect;
+	}
+	public String getPlatform() {
+		return platform;
+	}
+	public void setPlatform(int i) {
+		switch (i) {
+		case 0:
+			platform = "desktop";
+			break;
+		case 1:
+			platform = "web";
+			break;
+		case 2:
+			platform = "android";
+			break;
+		}
 	}
 	
 }
